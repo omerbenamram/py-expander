6e253fc3a02e99219898df54277fe0b451d05faf
Ohad Ravid <ohad.rv@gmail.com>
2013-09-21 13:46:23 +0300

    Changed variable names

--- src/pyexpander/transmission.py
+++ src/pyexpander/transmission.py
@@ -1,15 +1,18 @@
-__author__ = 'Ohad'
+__author__ = 'ohadr'
+
+
+import os
 
 
 def get_environmental_variables_from_transmission():
     """
     Return the environmental variables passed by tranmission to the script.
 
-    :return: (torrent_dir, torrent_name)
+    :return: (full_torrent_path, torrent_name)
     """
-    TARGET_TORRENT_DIR = os.getenv('TR_TORRENT_DIR')
-    TARGET_TORRENT_NAME = os.getenv('TR_TORRENT_NAME')
-    TARGET_TORRENT_DIR = os.path.realpath(os.path.join(TARGET_TORRENT_DIR, TARGET_TORRENT_NAME))
+    torrent_dir = os.getenv('TR_TORRENT_DIR')
+    torrent_name = os.getenv('TR_TORRENT_NAME')
+    full_path = os.path.realpath(os.path.join(torrent_dir, torrent_name))
 
-    return TARGET_TORRENT_DIR, TARGET_TORRENT_NAME
+    return full_path, torrent_name
 
