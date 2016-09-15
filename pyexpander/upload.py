a63195a80a0c4eefb34a23f5dbc6c0124fb9790a
ofir123 <ofirbrukner@gmail.com>
2016-09-15 03:26:37 +0300

    Set overwrite to true.

--- pyexpander/upload.py
+++ pyexpander/upload.py
@@ -88,7 +88,7 @@ def upload_file(file_path):
             current_dir += '/{}'.format(directory)
             subprocess.run('{} mkdir "{}"'.format(config.ACD_CLI_PATH, current_dir), shell=True)
         # Upload!
-        process = subprocess.run('{} upload "{}" "{}"'.format(config.ACD_CLI_PATH, new_path, cloud_dir), shell=True)
+        process = subprocess.run('{} upload -o "{}" "{}"'.format(config.ACD_CLI_PATH, new_path, cloud_dir), shell=True)
         # Check results.
         if process.returncode != 0:
             logger.error('Bad return code ({}) for file: {}'.format(process.returncode, file_path))
